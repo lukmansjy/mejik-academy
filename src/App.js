758ad4bcdc6849b9cd55b684e6eb095ce8534003
lukmansjy <lukman.rocks@live.com>
2020-05-07 13:22:03 +0700

    Course Page

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './assets/styles/App.css'
 import Login from './containers/pages/Login'
 import Home from './containers/pages/Home'
+import Course from './containers/pages/Course'
 
 const App = ()=>{
     return(
         <div>
             {/* <Login/> */}
-            <Home/>
+            {/* <Home/> */}
+            <Course/>
         </div>
     )
 }
---
+++ src/containers/pages/Course.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import NavBar from '../../components/molecules/NavBar'
+import ListCourse from '../organisms/ListCourse'
+
+const Course = ()=>{
+    return(
+        <div>
+            <NavBar/>
+            <ListCourse/>
+        </div>
+    )
+}
+
+export default Course
